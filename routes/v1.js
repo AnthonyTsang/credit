06d0f3fe02f8f8cebcb7a7e149f7af483e8051cd
Anthony Tsang <yeuksing@gmail.com>
2015-05-02 05:18:41 +0800

    add endpoint for get user info

--- routes/v1.js
+++ routes/v1.js
@@ -2,8 +2,10 @@ var router = require('express').Router();
 
 var index    = require('./routes/index');
 var register = require('./routes/user/register');
+var user     = require('./routes/user');
 
 router.get( '/', index);
 router.post('/register', register);
+router.get( '/user/:id', user)
 
 module.exports = router;
