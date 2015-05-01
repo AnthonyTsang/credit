292b1e73a105fdf998b4545d25afa92475d56037
Anthony Tsang <yeuksing@gmail.com>
2015-05-02 05:21:29 +0800

    add endpoint for deposit

--- routes/v1.js
+++ routes/v1.js
@@ -3,9 +3,11 @@ var router = require('express').Router();
 var index    = require('./routes/index');
 var register = require('./routes/user/register');
 var user     = require('./routes/user');
+var userDeposit = require('./routes/user/deposit')
 
 router.get( '/', index);
 router.post('/register', register);
-router.get( '/user/:id', user)
+router.get( '/user/:id', user);
+router.post('/user/:id/deposit', userDeposit);
 
 module.exports = router;
